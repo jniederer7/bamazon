dd1116d12db0cff0c1a8adbe25aa1f2683a62b3d
unknown <jniederer7@gmail.com>
2017-11-17 21:11:53 -0500

    added pictures

--- bamazon.js
+++ bamazon.js
@@ -20,7 +20,7 @@ var prompt = require("prompt");
 var connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: '',
+  password: '4dollars',
   database: 'Bamazon'
 });
 
